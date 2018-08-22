5880d8020a7b19886a0c04b435effc95c65dc258
wangyujue <108474309@qq.com>
2018-08-22 20:50:26 +0800

    添加文本框功能完成 保存功能bug修复

---
+++ assets/js/drag-actions.js
@@ -0,0 +1,95 @@
+// 保存位置信息事件
+$("#chart_save").click(function(){
+	var itemsInfo=[];
+	var items=$("#drag_content>.drag_item");
+	for(var i=0;i<items.length;i++){
+		var info=getItemInfo(items[i]);
+		itemsInfo.push(info);
+	}
+	$("#drag_item_info_arr").val("["+itemsInfo+"]");
+	alert("保存成功");
+})
+// 初始化历史记录
+$("#chart_return").click(function(){
+	initFn();
+});
+// 切换网格背景
+$("#chart_grid").click(function(){
+	$(".drag_content").toggleClass("drag_content_grid");
+})
+
+// 添加区域位置
+$("#chart_bigger").click(function(){
+	var height=$(".drag_container").height();
+	$(".drag_container").css("height",height+50)
+})
+$("#chart_smaller").click(function(){
+	var height=$(".drag_container").height();
+	if(height>=650){
+		$(".drag_container").css("height",height-50)
+	}else{
+		alert("已经是最小值！")
+	}
+})
+// 初始化页面
+function initFn(){
+	var saveData=$("#drag_item_info_arr").val();
+	console.log(saveData)
+	console.log(typeof saveData)
+	if(saveData==""){
+		alert("无保存信息");
+		return;
+	}
+	saveData=JSON.parse(saveData);
+	// console.log(saveData)
+	$("#drag_content").html("");
+	for (var i = 0; i < saveData.length; i++) {
+		var dragItem=document.createElement("div");
+		dragItem.id=saveData[i].id;
+		if(saveData[i].type=="text"){
+			//初始化文本框
+			dragItem.className="drag_item text_type";
+			dragItem.innerHTML='<div class="drag_item_header">'+
+			'<input type="hidden" class="echart_data"/>'+
+			'<div class="drag_item_move"></div>'+
+			'<div class="drag_item_header_action_list">'+
+			'<i class="fa fa-times close_btn" data-type="close" data-pid="' + saveData[i].id + '" aria-hidden="true"></i>'+
+			'</div></div><div class="drag_item_box" id="'+saveData[i].id+'_div'+'"><textarea class="text_val"></textarea></div>';
+			$("#drag_content").append(dragItem);
+				console.log(saveData[i])
+				$(dragItem).find(".text_val").val(saveData[i].title)
+		}else{
+			// 初始换echarts图形
+			dragItem.className="drag_item chart_type1";
+			dragItem.innerHTML='<div class="drag_item_header">'+
+			'<input type="hidden" class="echart_data"/>'+
+			'<div class="drag_item_move"></div>'+
+			'<input class="title" value="'+saveData[i].id+'" disabled="disabled">'+
+			'<div class="drag_item_header_action_list">'+
+			'<i class="fa fa-window-minimize minsize_btn" data-type="min" data-pid="' + saveData[i].id + '" aria-hidden="true"></i>'+
+			'<i class="fa fa-window-restore maxsize_btn" data-type="max" data-pid="' + saveData[i].id + '" aria-hidden="true"></i>'+
+			'<i class="fa fa-times close_btn" data-type="close" data-pid="' + saveData[i].id + '" aria-hidden="true"></i>'+
+			'</div></div><div class="drag_item_box" id="'+saveData[i].id+'_div'+'"></div>';
+			$("#drag_content").append(dragItem);
+			drawCharts(saveData[i].id+'_div',saveData[i].type);
+		}
+		
+		$(dragItem).css({"height":saveData[i].height,"left":saveData[i].left,
+						"top":saveData[i].top,"width":saveData[i].width,"position":"absolute"})
+		updateChartInfo(dragItem);
+		// 填充echarts信息
+	}
+	drag();
+	bindEvent();
+};
+
+// 鼠标右键功能
+window.oncontextmenu=function(e){
+	//取消默认的浏览器自带右键 很重要！！
+	e.preventDefault();
+	console.log(e.target)
+	if($(e.target).hasClass("drag_item_move")){
+		console.log("d")
+	}
+
+}
--- assets/js/drag-charts.js
+++ assets/js/drag-charts.js
@@ -5,18 +5,8 @@ function drag(){
 		containment: "#drag_content",
 		opacity:0.35,
 		start: function() {
-
       	},
         drag: function(e) {
-        	// var info = JSON.parse($(this).find(".echart_data").val());
-        	// var bottom = parseInt(info.bottom);
-        	// //console.log(bottom);
-
-        	// if(bottom <= 0){
-        	// 	$(".drag_container").css("height", ($(".drag_container").height() * 1 + 50) + 'px');
-        	// }
-        	// updateChartInfo(this);
-        	// console.log(typeof );
         },
         stop: function() {
         	// console.log(this);
@@ -26,11 +16,10 @@ function drag(){
 	$( ".drag_item" ).resizable({
       minHeight: 50,
       minWidth: 50,
-      // n，e，s，w，ne，se，sw，nw
       handles:"se,sw,nw,ne,n,e,s,w",
       start: function() {
       
-      	},
+      },
       resize:function(){
 
       },	
@@ -45,14 +34,24 @@ drag();
 var barChartNum=0;
 var lineChartNum=0;
 var scatterChartNum=0;
+var textNum=0;
 
 var addBarChart=document.getElementById("add_bar_chart");
 var addLineChart=document.getElementById("add_line_chart");
 var addScatterChart=document.getElementById("add_scatter_chart");
+var addText=document.getElementById("add_text");
+
+$(addBarChart).click(function(){createChart("bar")})
+$(addLineChart).click(function(){createChart("line")})
+$(addScatterChart).click(function(){createChart("scatter")})
+$(addText).click(function(){createShape("text")})
+
 
 dragShowChartsFn(addBarChart,"bar");
 dragShowChartsFn(addLineChart,"line")
 dragShowChartsFn(addScatterChart,"scatter")
+dragShowChartsFn(addText,"text")
+
 function dragShowChartsFn(obj,type){
 	obj.ondragstart=function(){ // 拖拽开始
     // console.log('start');
@@ -61,52 +60,15 @@ function dragShowChartsFn(obj,type){
 	obj.ondragend=function(e){ // 当拖拽结束 ，清空temp
 	    var x0=parseInt($("#drag_content").offset().left);
 	    var y0=parseInt($("#drag_content").offset().top)
-		createChart(type,(e.clientX-x0),(e.clientY-y0));	
+	    if(type=="text"){
+	    	createShape(type,(e.clientX-x0),(e.clientY-y0))
+	    }else{
+			createChart(type,(e.clientX-x0),(e.clientY-y0));	
+	    }
 	}
 }
 
-$("#add_bar_chart").click(function(){
-	createChart('bar');	
-})
-$("#add_line_chart").click(function(){
-	createChart('line');	
-})
-$("#add_scatter_chart").click(function(){
-	createChart('scatter');	
-})
-// 保存位置信息事件
-$("#chart_save").click(function(){
-	var itemsInfo=[];
-	var items=$("#drag_content>.drag_item");
-	for(var i=0;i<items.length;i++){
-		var info=getItemInfo(items[i]);
-		itemsInfo.push(info);
-	}
-	$("#drag_item_info_arr").val("["+itemsInfo+"]");
-	alert("保存成功");
-})
-// 初始化历史记录
-$("#chart_return").click(function(){
-	initFn();
-});
-// 切换网格背景
-$("#chart_grid").click(function(){
-	$(".drag_content").toggleClass("drag_content_grid");
-})
 
-// 添加区域位置
-$("#chart_bigger").click(function(){
-	var height=$(".drag_container").height();
-	$(".drag_container").css("height",height+50)
-})
-$("#chart_smaller").click(function(){
-	var height=$(".drag_container").height();
-	if(height>=650){
-		$(".drag_container").css("height",height-50)
-	}else{
-		alert("已经是最小值！")
-	}
-})
 
 // 创建echarts
 function createChart(type,left,top){
@@ -240,63 +202,55 @@ function bindEvent(){
 	});
 }
 
-// 初始化页面
-function initFn(){
-	var saveData=$("#drag_item_info_arr").val();
-	console.log(saveData)
-	console.log(typeof saveData)
-	if(saveData==""){
-		alert("无保存信息");
-		return;
+// 创造形状
+function createShape(type,left,top){
+	var chartNum;
+	var chartType1 = document.createElement("div");
+	chartType1.style.background="inherit";
+	chartType1.style.left=left+"px";
+	chartType1.style.top=top+"px";
+	// var chartName;
+	switch(type)
+	{
+	case 'text':
+	  textNum++;
+	  chartNum="text_shape_"+barChartNum;
+	  chartType1.id=chartNum;
+	  chartType1.className="drag_item text_type";
+	  chartType1.innerHTML='<div class="drag_item_header">'+
+	'<input type="hidden" class="echart_data"/>'+
+	'<div class="drag_item_move"></div>'+
+	'<div class="drag_item_header_action_list">'+
+	'<i class="fa fa-times close_btn" data-type="close" data-pid="' + chartNum + '" aria-hidden="true"></i>'+
+	'</div></div><div class="drag_item_box" id="'+chartNum+'_div'+'"><textarea class="text_val"></textarea></div>';
+	  break;
+	default:
+	  break;
 	}
-	saveData=JSON.parse(saveData);
-	// console.log(saveData)
-	$("#drag_content").html("");
-	for (var i = 0; i < saveData.length; i++) {
-		// saveData[i]=JSON.parse(saveData[i]);
-		console.log(typeof saveData[i])
-		var dragItem=document.createElement("div");
-		dragItem.className="drag_item chart_type1";
-		dragItem.id=saveData[i].id;
-		dragItem.innerHTML='<div class="drag_item_header">'+
-		'<input type="hidden" class="echart_data"/>'+
-		'<div class="drag_item_move"></div>'+
-		'<input class="title" value="'+saveData[i].id+'" disabled="disabled">'+
-		'<div class="drag_item_header_action_list">'+
-		'<i class="fa fa-window-minimize minsize_btn" data-type="min" data-pid="' + saveData[i].id + '" aria-hidden="true"></i>'+
-		'<i class="fa fa-window-restore maxsize_btn" data-type="max" data-pid="' + saveData[i].id + '" aria-hidden="true"></i>'+
-		'<i class="fa fa-times close_btn" data-type="close" data-pid="' + saveData[i].id + '" aria-hidden="true"></i>'+
-		'</div></div><div class="drag_item_box" id="'+saveData[i].id+'_div'+'"></div>';
-		$(dragItem).css({"height":saveData[i].height,"left":saveData[i].left,
-						"top":saveData[i].top,"width":saveData[i].width,"position":"absolute"})
-		$("#drag_content").append(dragItem);
-
-		// 填充echarts信息
-		drawCharts(saveData[i].id+'_div',saveData[i].type);
-	}
+	$("#drag_content").append(chartType1);
+	$(chartType1).find(".text_val").change(function(){
+		updateChartInfo(chartType1);
+	})
+	updateChartInfo(chartType1);
 	drag();
 	bindEvent();
-};
-
-// 鼠标右键功能
-window.oncontextmenu=function(e){
-	//取消默认的浏览器自带右键 很重要！！
-	e.preventDefault();
-	console.log(e.target)
-	if($(e.target).hasClass("drag_item_move")){
-		console.log("d")
-	}
-
 }
+
 
 
 // 更新图表信息
 function updateChartInfo(obj){
+	console.log('update')
+	console.log(obj)
 	var data={};
-
 	data.type = $(obj).attr("id").split("_")[0];
 	data.id = $(obj).attr("id");
-	data.title = $(obj).find(".title").val();
+	if(data.type=="text"){
+		console.log($(obj).find(".text_val").val())
+		data.title=$(obj).find(".text_val").val();
+	}else{
+		data.title = $(obj).find(".title").val();
+	}
 	data.height = $(obj).height();
 	data.width = $(obj).width();
 	data.top = $(obj).css("top");
